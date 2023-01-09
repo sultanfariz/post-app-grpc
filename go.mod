99bc0ddb7752358fd27a50dbe872fe07eced79c5
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2023-01-09 15:29:33 +0700

    feat: add db driver for mysql

--- go.mod
+++ go.mod
@@ -3,9 +3,10 @@ module github.com/sultanfariz/simple-grpc
 go 1.16
 
 require (
-	github.com/golang/protobuf v1.5.2 // indirect
 	golang.org/x/net v0.4.0 // indirect
 	google.golang.org/genproto v0.0.0-20221207170731-23e4bf6bdc37 // indirect
-	google.golang.org/grpc v1.51.0 // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
+	google.golang.org/grpc v1.51.0
+	google.golang.org/protobuf v1.28.1
+	gorm.io/driver/mysql v1.4.5
+	gorm.io/gorm v1.24.3
 )
---
+++ repository/mysql/drivers.go
@@ -0,0 +1,28 @@
+package mysql
+
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func InitDB() *gorm.DB {
+	config := map[string]string{
+		"DB_USERNAME": "root",
+		"DB_PASSWORD": "",
+		"DB_HOST":     "localhost",
+		"DB_PORT":     "3306",
+		"DB_NAME":     "simple_grpc",
+	}
+
+	connection := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", config["DB_USERNAME"], config["DB_PASSWORD"], config["DB_HOST"], config["DB_PORT"], config["DB_NAME"])
+
+	DB, err := gorm.Open(mysql.Open(connection), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	DB.AutoMigrate()
+	return DB
+}
