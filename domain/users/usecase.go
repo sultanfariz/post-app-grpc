17b9ac57dc070e4c7c13e3195a1867b38f177bfe
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2023-01-18 11:24:49 +0700

    feat(user): add login usecase

--- domain/users/usecase.go
+++ domain/users/usecase.go
@@ -5,18 +5,23 @@ import (
 	"errors"
 	"time"
 
+	"github.com/sultanfariz/simple-grpc/infrastructure/commons"
 	"golang.org/x/crypto/bcrypt"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type UsersUsecase struct {
 	UsersRepository UsersRepositoryInterface
 	ContextTimeout  time.Duration
+	jwtConfig       *commons.ConfigJWT
 }
 
-func NewUsersUsecase(ur UsersRepositoryInterface, timeout time.Duration) *UsersUsecase {
+func NewUsersUsecase(ur UsersRepositoryInterface, timeout time.Duration, jwtConfig *commons.ConfigJWT) *UsersUsecase {
 	return &UsersUsecase{
 		UsersRepository: ur,
 		ContextTimeout: timeout,
+		jwtConfig: jwtConfig,
 	}
 }
 
@@ -40,4 +45,24 @@ func (uu *UsersUsecase) Register(ctx context.Context, user *User) (*User, error)
 	}
 
 	return user, nil
+}
+
+func (uu *UsersUsecase) Login(ctx context.Context, user *User) (string, error) {
+	ctx, cancel := context.WithTimeout(ctx, uu.ContextTimeout)
+	defer cancel()
+
+	if user.Email == "" || user.Password == "" {
+		return "", status.Errorf(codes.InvalidArgument, "invalid email or password")
+	}
+
+	user, err := uu.UsersRepository.Login(ctx, user.Email, user.Password)
+	if err != nil {
+		return "", err
+	}
+	token, err := uu.jwtConfig.GenerateToken(user.Id, user.Email)
+	if err != nil {
+		return "", status.Errorf(codes.Internal, err.Error())
+	}
+
+	return token, nil
 }
