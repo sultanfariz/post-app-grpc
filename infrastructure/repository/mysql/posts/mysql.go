1568cae044f5a58b72d42c6935845e896d221129
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2023-01-21 00:49:47 +0700

    feat: implement repository for post

---
+++ infrastructure/repository/mysql/posts/mysql.go
@@ -0,0 +1,49 @@
+package posts
+
+import (
+	"context"
+	"time"
+
+	model "github.com/sultanfariz/simple-grpc/domain/posts"
+
+	"gorm.io/gorm"
+)
+
+type PostsRepository struct {
+	DBConnection *gorm.DB
+}
+
+func NewPostsRepository(db *gorm.DB) *PostsRepository {
+	return &PostsRepository{
+		DBConnection: db,
+	}
+}
+
+func (r *PostsRepository) GetAll(ctx context.Context) ([]*model.Post, error) {
+	data := []*model.Post{}
+	if err := r.DBConnection.Find(&data).Error; err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func (r *PostsRepository) GetById(ctx context.Context, id int) (*model.Post, error) {
+	data := model.Post{}
+	if err := r.DBConnection.Where("id = ?", id).First(&data).Error; err != nil {
+		return nil, err
+	}
+
+	return &data, nil
+}
+
+func (r *PostsRepository) Insert(ctx context.Context, post *model.Post) (*model.Post, error) {
+	post.CreatedAt = time.Now()
+	post.UpdatedAt = time.Now()
+
+	if err := r.DBConnection.Create(&post).Error; err != nil {
+		return nil, err
+	}
+
+	return post, nil
+}
