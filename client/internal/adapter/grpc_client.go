0f536865c5209c8249fe1f1b20f1483930d434c8
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2023-02-03 11:26:58 +0700

    feat(client): adapter client for grpc server

---
+++ client/internal/adapter/grpc_client.go
@@ -0,0 +1,49 @@
+package adapter
+
+import (
+	"context"
+	"errors"
+
+	"github.com/sultanfariz/simple-grpc/interface/grpc/post"
+)
+
+type PostClient struct {
+	postClient post.PostServiceClient
+}
+
+func NewPostClient(postClient post.PostServiceClient) *PostClient {
+	return &PostClient{
+		postClient: postClient,
+	}
+}
+
+func (p PostClient) GetAllPosts(ctx context.Context) ([]*post.Post, error) {
+	posts, err := p.postClient.GetAllPosts(ctx, &post.GetAllPostsRequest{
+		Page:  1,
+		Limit: 10,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	if posts.Meta.Status != "success" {
+		return nil, errors.New(posts.Meta.Message)
+	}
+
+	return posts.Posts, nil
+}
+
+func (p PostClient) GetPostById(ctx context.Context, id int32) (*post.Post, error) {
+	post, err := p.postClient.GetPostById(ctx, &post.GetPostByIdRequest{
+		Id: id,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	if post.Meta.Status != "success" {
+		return nil, errors.New(post.Meta.Message)
+	}
+
+	return post.Post, nil
+}
