eda8fb4481eb81eae3a21e88335a64e04a320f64
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2023-02-10 15:13:32 +0700

    chore(client): init connection to rabbitmq

--- client/cmd/grpc.go
+++ client/cmd/grpc.go
@@ -4,11 +4,13 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"log"
 	"net"
 
 	"github.com/sultanfariz/simple-grpc/client/internal/adapter"
 	"github.com/sultanfariz/simple-grpc/client/internal/transport"
 	"github.com/sultanfariz/simple-grpc/interface/grpc/post"
+	"github.com/wagslane/go-rabbitmq"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/reflection"
@@ -35,10 +37,20 @@ func (g *gRPCServer) Start() error {
 		return errors.New("did not connect: %v" + err.Error())
 	}
 
+	// start connection to RabbitMQ
+	amqpConn, err := rabbitmq.NewConn(
+		"amqp://guest:guest@localhost",
+		rabbitmq.WithConnectionOptionsLogging,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
 	server := grpc.NewServer()
 	postServiceClient := post.NewPostServiceClient(conn)
 	postClient := adapter.NewPostClient(postServiceClient)
-	transport.NewPostHandler(server, *postClient)
+	transport.NewPostHandler(server, *postClient, amqpConn)
 
 	// Start gRPC server
 	lis, err := net.Listen("tcp", g.clientPort)
--- client/internal/transport/rest_api.go
+++ client/internal/transport/rest_api.go
@@ -2,20 +2,24 @@ package transport
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/sultanfariz/simple-grpc/client/internal/adapter"
 	postPB "github.com/sultanfariz/simple-grpc/client/pkg/grpc/post"
+	rabbitmq "github.com/wagslane/go-rabbitmq"
 	"google.golang.org/grpc"
 )
 
 type PostHandler struct {
 	PostClient adapter.PostClient
+	conn       *rabbitmq.Conn
 	postPB.UnimplementedPostClientServiceServer
 }
 
-func NewPostHandler(gserver *grpc.Server, postClient adapter.PostClient) {
+func NewPostHandler(gserver *grpc.Server, postClient adapter.PostClient, conn *rabbitmq.Conn) {
 	postHandler := &PostHandler{
 		PostClient: postClient,
+		conn:       conn,
 	}
 	postPB.RegisterPostClientServiceServer(gserver, *postHandler)
 }
@@ -65,4 +69,69 @@ func (p PostHandler) GetPostById(ctx context.Context, in *postPB.GetPostByIdRequ
 			UpdatedAt: post.UpdatedAt,
 		},
 	}, nil
+}
+
+func (p PostHandler) SubscribePostByTopic(in *postPB.SubscribePostByTopicRequest, stream postPB.PostClientService_SubscribePostByTopicServer) error {
+	// func (p PostHandler) SubscribePostByTopic(ctx context.Context, in *postPB.SubscribePostByTopicRequest) (postPB.PostClientService_SubscribePostByTopicClient, error) {
+	// func (p PostHandler) SubscribePostByTopic(in *postPB.SubscribePostByTopicRequest) (*postPB.PostClientService_SubscribePostByTopicClient, error) {
+	topic := in.GetTopic()
+	fmt.Println("topic: ", topic)
+	consumer, err := rabbitmq.NewConsumer(
+		p.conn,
+		// func(d rabbitmq.Delivery) (action rabbitmq.Action) {
+		func(d rabbitmq.Delivery) rabbitmq.Action {
+			// post := &Post{}
+			// err := json.Unmarshal(d.Body, post)
+			// if err != nil {
+			// 	log.Println(err)
+			// 	return rabbitmq.Reject
+			// }
+
+			return rabbitmq.Ack
+		},
+		// fmt.Sprintf("post:%s", topic),
+		"grpc_queue",
+		rabbitmq.WithConsumerOptionsRoutingKey("grpc_queue"),
+		rabbitmq.WithConsumerOptionsExchangeName("events"),
+		rabbitmq.WithConsumerOptionsExchangeDeclare,
+	)
+	if err != nil {
+		return err
+		// return nil, err
+	}
+	defer consumer.Close()
+
+	// sigs := make(chan os.Signal, 1)
+	// done := make(chan bool, 1)
+	// signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	// <-sigs
+
+	// go func() {
+	// 	sig := <-sigs
+	// 	fmt.Println()
+	// 	fmt.Println(sig)
+	// 	done <- true
+	// }()
+
+	// fmt.Println("awaiting signal")
+	// <-done
+	// fmt.Println("stopping consumer")
+
+	return nil
+	// return nil, nil
+
+	// return &postPB.SubscribePostByTopicResponse{
+	// 	Meta: &postPB.GenericResponse{
+	// 		Status:  "success",
+	// 		Message: "success",
+	// 	},
+	// 	// Post: &postPB.Post{
+	// 	// 	Id:        post.Id,
+	// 	// 	Title:     post.Title,
+	// 	// 	Content:   post.Content,
+	// 	// 	Topic:     post.Topic,
+	// 	// 	CreatedAt: post.CreatedAt,
+	// 	// 	UpdatedAt: post.UpdatedAt,
+	// 	// },
+	// }, nil
 }
